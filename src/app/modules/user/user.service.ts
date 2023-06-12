56272684b516ffaaabfe549f8ca3057af937f128
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-06-12 18:02:29 +0600

    test

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,28 +1,28 @@
-import config from '../../../config'
-import ApiError from '../../../errors/ApiError'
-import { IUser } from './user.interface'
-import { User } from './user.model'
-import { generateUserId } from './user.utils'
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
+import { IUser } from './user.interface';
+import { User } from './user.model';
+import { generateUserId } from './user.utils';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generate increment id
-  const id = await generateUserId()
+  const id = await generateUserId();
 
-  user.id = id
+  user.id = id;
 
   // default password
   if (!user.password) {
-    user.password = config.default_user_password as string
+    user.password = config.default_user_password as string;
   }
 
-  const createdUser = User.create(user)
+  const createdUser = User.create(user);
   if (!createdUser) {
-    throw new ApiError(400, 'Failed to create user')
+    throw new ApiError(400, 'Failed to create user');
   }
 
-  return createdUser
-}
+  return createdUser;
+};
 
 export const UserService = {
   createUser,
-}
+};
